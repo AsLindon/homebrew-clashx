7bbb63ab5aad49cf647fe4d29e3e4681931679f3
ø <62425376+AsLindon@users.noreply.github.com>
2026-06-22 16:49:00 +0800

    Update ClashX Pro version to 1.138.0

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,5 +1,5 @@
 cask "clashx-pro" do
-  version "1.139.0"
+  version "1.138.0"
   sha256 "e6f3e51b0bc8e7a4a3a4c0f14cee327a7e62e924c763d9cfd3fd6a40a52f01a9"
 
   url "https://github.com/ClashX-Pro/ClashX/releases/download/#{version}/ClashX.dmg"
